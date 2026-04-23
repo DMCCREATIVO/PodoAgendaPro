f02d68ea9516d3bb658173249b0f27128e105079
softgenai <admin@softgen.ai>
2026-04-23 22:54:32 +0000

    refactor(frontend): wipe legacy code for clean start

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,13 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "@/contexts/ThemeProvider";
-import { CompanyProvider } from "@/contexts/CompanyContext";
 import { Toaster } from "@/components/ui/toaster";
 
 export default function App({ Component, pageProps }: AppProps) {
-  // COMPLETAMENTE LIMPIO - Sin verificaciones de auth
-  // Sin listeners - Sin nada que pueda interferir
-  
   return (
-    <ThemeProvider>
-      <CompanyProvider>
-        <Component {...pageProps} />
-        <Toaster />
-      </CompanyProvider>
+    <ThemeProvider defaultTheme="light" storageKey="podoagenda-theme">
+      <Component {...pageProps} />
+      <Toaster />
     </ThemeProvider>
   );
 }
